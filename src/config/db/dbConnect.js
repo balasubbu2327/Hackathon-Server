b21ff6815f510c89ca794fc875b6d9d18f1d8339
Bala Subramanian <98680016+balasubbu2327@users.noreply.github.com>
2022-06-14 12:23:33 +0530

    Update dbConnect.js

--- src/config/db/dbConnect.js
+++ src/config/db/dbConnect.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
-      useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
